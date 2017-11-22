9f3bd70fc24eba35dbbf3e827194aff765c5664f
7R <4a2k0055@stust.edu.tw>
2017-11-22 08:54:01 +0800

    Initial commit

---
+++ app/src/main/java/demo/com/mybroadcastreceiver/BroadcastReceiver1.java
@@ -0,0 +1,22 @@
+package demo.com.mybroadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by auser on 2017/11/21.
+ */
+
+public class BroadcastReceiver1 extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String event = intent.getAction();
+        if(event.equals("demo.com.intent.action.CUSTOM_BROADCAST"))
+            Toast.makeText(context, "BR1:收到自訂廣播", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(context, "BR1:" + event, Toast.LENGTH_SHORT).show();
+
+    }
+}
---
+++ app/src/main/java/demo/com/mybroadcastreceiver/BroadcastReceiver2.java
@@ -0,0 +1,21 @@
+package demo.com.mybroadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by auser on 2017/11/21.
+ */
+
+public class BroadcastReceiver2 extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        String event = intent.getAction();
+        Toast.makeText(context, "BR2:" + event, Toast.LENGTH_SHORT).show();
+    }
+}
+
+
